4f6ce7cfe5fe07564287d4fccabbf9aa3aa8a34b
R-karina <reilyanu_karina@mail.ru>
2025-05-26 22:05:04 +0300

    Update FoodFactory.java

--- src/FoodFactory.java
+++ src/FoodFactory.java
@@ -1,7 +1,6 @@
 //Реализует интерфейс `ProductFactory` для создания продуктов питания.
 
 class FoodFactory {
-    @Override
     public Product createProduct(String name, double price, double weight) {
         return new Food(name, price, weight);
     }
